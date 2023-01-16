f720be0631465b57149d1754003a4fb6feba64a4
Saurabh Patel <saurabh.devstree@gmail.com>
2023-01-17 01:00:46 +0530

    - remove category object

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -26,7 +26,7 @@ class CategoryController extends Controller
 
     public function list(Request $request){
         try{
-            $categories =  Category::with('categoryImage','subCategory','shops');
+            $categories =  Category::with('subCategory','shops');
             $categoryCount =  $categories->count();
             if($categoryCount <= 0){
                 $response['categories'] = [];
