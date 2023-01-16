84f5085bbdc810f2156d8e571d4c65c63ce668f6
Saurabh Patel <saurabh.devstree@gmail.com>
2023-01-17 00:06:53 +0530

    - add full path url

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
+
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute(){
+        $path =  '/images/product/' . $this->product_id . "/";
+        return ($this->image) ? asset($path.$this->image) : "";
+    }
 }
