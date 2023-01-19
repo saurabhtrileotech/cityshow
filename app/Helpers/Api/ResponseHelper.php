d816501a7711a3cff2523aff8f5076e60bf2d80e
Saurabh Patel <saurabh.devstree@gmail.com>
2023-01-19 22:19:21 +0530

    -  remove data param from error message

--- app/Helpers/Api/ResponseHelper.php
+++ app/Helpers/Api/ResponseHelper.php
@@ -19,7 +19,7 @@ class ResponseHelper
             'status' => $statusCode,
             "message" => $errorMessage,
             "success" => $success,
-            "data" => $data
+            // "data" => $data
         ], $statusCode);
     }
 
